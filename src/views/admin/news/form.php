19e1f3c10d4edcd06679858b5e589eac60b8f221
Alexia-Gomez <alexiaareg_23@alu.uabcs.mx>
2025-11-25 09:38:47 -0700

    Front: edición form.php
    Se agregaron campos de fecha y duración

--- src/views/admin/news/form.php
+++ src/views/admin/news/form.php
@@ -21,23 +21,38 @@
         <input type="text"
           name="title"
           required
-          class="mt-1 w-full border border-gray-200 rounded-md p-2 focus:border-blue-900 outline-none">
+          class="mt-1 w-full border border-gray-300 rounded-md p-2 focus:border-blue-900 outline-none">
+
+        <div class="flex mt-4 justify-between">
+          <div>
+            <label class="block text-gray-700 font-medium">Fecha</label>
+            <input type="date" name="fecha" required value="<?= $newsItem->fecha ?? date('Y-m-d') ?>"
+              class="mt-1 w- border border-gray-300 rounded-md p-2 focus:border-blue-900 outline-none">
+          </div>
+          <div>
+            <label class="block text-gray-700 font-medium">Duración</label>
+            <input type="text"
+            name="title"
+          required
+          class="mt-1 w-full border border-gray-300 rounded-md p-2 focus:border-blue-900 outline-none">
+          </div>
+        </div>
 
         <label class="block mt-4 text-gray-700 font-medium">Entradilla</label>
         <textarea name="lead"
           required
-          class="mt-1 w-full border border-gray-200 rounded-md p-2 h-15 resize-none focus:border-blue-900 outline-none"></textarea>
+          class="mt-1 w-full border border-gray-300 rounded-md p-2 h-15 resize-none focus:border-blue-900 outline-none"></textarea>
 
         <label class="block mt-4 text-gray-700 font-medium">Cuerpo</label>
         <textarea name="body"
           required
-          class="mt-1 w-full border border-gray-200 rounded-md p-2 h-25 resize-none focus:border-blue-900 outline-none"></textarea>
+          class="mt-1 w-full border border-gray-300 rounded-md p-2 h-25 resize-none focus:border-blue-900 outline-none"></textarea>
 
         <label class="block mt-4 text-gray-700 font-medium">Imagen (opcional)</label>
         <input type="file"
           name="image"
           accept="image/*"
-          class="mt-1 w-full border border-gray-200 rounded-md p-2 bg-white cursor-pointer focus:ring-blue-900 focus:border-blue-900 outline-none">
+          class="mt-1 w-full border border-gray-300 rounded-md p-2 bg-white cursor-pointer focus:ring-blue-900 focus:border-blue-900 outline-none">
 
         <button type="submit"
           class="mt-5 bg-blue-800 text-white px-5 py-2 rounded-md hover:bg-blue-900 transition">
