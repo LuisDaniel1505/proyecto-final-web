955399f128c7dd369785bf11981bb62b0d3f15a8
LuisDaniel1505 <lontiveros_23@alu.uabcs.mx>
2025-11-28 07:20:42 -0700

    Fix: Link

--- src/views/layouts/header.php
+++ src/views/layouts/header.php
@@ -29,7 +29,7 @@
         </div>
       </div>
       <div class="navbar-center">
-        <a class="btn btn-ghost text-xl font-bold text-white">Imperio Azul Noticias</a>
+        <a class="btn btn-ghost text-xl font-bold text-white" href="<?=BASE_PATH?>/index.php">Imperio Azul Noticias</a>
       </div>
       <div class="navbar-end">
         <button class="btn btn-ghost btn-circle">
